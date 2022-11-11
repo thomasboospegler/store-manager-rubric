8d4ac90862e0a469fbae65404db601481e0fe018
Thomas Boos Pegler <tboospegler@gmail.com>
2022-11-11 17:39:24 -0400

    corrige connection.js

--- src/models/database/connection.js
+++ src/models/database/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: 'StoreManager',
+  port: process.env.MYSQL_PORT || 3306,
 });
 
 module.exports = connection;
